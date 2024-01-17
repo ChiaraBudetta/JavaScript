00b4dcd7d17fe8bfade3a8a46ba7393a06c1adc9
Chiara B <budettachiara@gmail.com>
2024-01-17 12:38:04 -0300

    tercera pre-entrega

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
 let cantidadProductos = 0;
-let productosEnCarrito = [];
-const esEsc = productosEnCarrito.find(producto => producto.toLowerCase() === 'esc');
+let productosEnCarrito = JSON.parse(localStorage.getItem('productosEnCarrito')) || [];
 
 function incrementarContador() {
   cantidadProductos++;
 }
 
-
 function agregarAlCarrito() {
-  do {
-    let nombreProducto = prompt('Ingrese el nombre del producto para agregar al carrito (escriba "esc" para salir):');
-    
-    if (esEsc) {
-      alert(`Productos en el carrito:\n${productosEnCarrito.join('\n')}\n\nCantidad total de productos: ${cantidadProductos}\n\nGracias por usar el carrito de compras. Hasta luego.`);
-      return;
-    }
-    if (nombreProducto !== null && nombreProducto.trim() !== '' && !productosEnCarrito.includes(nombreProducto))  {
-      incrementarContador();
-        productosEnCarrito.push(nombreProducto);
-        alert(`"${nombreProducto}" se ha agregado al carrito.`);
-    } else if (nombreProducto && nombreProducto.toLowerCase() === 'esc') {
-      alert(`Productos en el carrito:\n${productosEnCarrito}\n\nCantidad total de productos: ${cantidadProductos}\n\nGracias por usar el carrito de compras. Hasta luego.`);
-      return;
-    }else {
-      alert('Por favor, ingrese un nombre de producto.');
-    }
-  } while (true); 
+  const nombreProductoInput = document.getElementById('nombreProducto');
+  const mensajeDiv = document.getElementById('mensaje');
+
+  let nombreProducto = nombreProductoInput.value;
+
+  if (nombreProducto.trim() !== '' && !productosEnCarrito.includes(nombreProducto)) {
+    incrementarContador();
+    productosEnCarrito.push(nombreProducto);
+    guardarEnLocalStorage();
+    mostrarMensaje(`"${nombreProducto}" se ha agregado al carrito.`);
+  } else {
+    mostrarMensaje('Por favor, ingrese un nombre de producto.');
+  }
+
+  nombreProductoInput.value = ''; // Limpiar el campo de entrada después de agregar al carrito
+}
+
+function mostrarMensaje(mensaje) {
+  const mensajeDiv = document.getElementById('mensaje');
+  mensajeDiv.innerText = mensaje;
+}
+
+function mostrarCarrito() {
+  mostrarMensaje(`Productos en el carrito:\n${productosEnCarrito.join('\n')}\n\nCantidad total de productos: ${cantidadProductos}\n\nGracias por usar el carrito de compras. Hasta luego.`);
+}
+
+function guardarEnLocalStorage() {
+  localStorage.setItem('productosEnCarrito', JSON.stringify(productosEnCarrito));
 }
 
 
-agregarAlCarrito();
+document.getElementById('agregarCarritoBtn').addEventListener('click', agregarAlCarrito);
+document.getElementById('mostrarCarritoBtn').addEventListener('click', mostrarCarrito);
